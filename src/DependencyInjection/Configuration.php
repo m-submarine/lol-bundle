48b58a94e345e23fb7a2db0cd4c460c04e40eb0d
mykees <mykeespro@gmail.com>
2022-03-07 11:55:22 +0100

    reorganize structure

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -13,7 +13,7 @@ class Configuration implements ConfigurationInterface
         $rootNode = $treeBuilder->getRootNode();
         $rootNode
             ->children()
-                ->scalarNode('tag_class')->defaultValue('App\Entity\Post')
+                ->scalarNode('tag_class')->defaultValue('App\Entity\Lol')
                 ->end()
                 ->booleanNode('purge')
                     ->defaultFalse()
