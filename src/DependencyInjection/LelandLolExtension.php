36a27fe5a8069d8ee7eab28d689eca6ee67a919e
mykees <mykeespro@gmail.com>
2022-03-07 12:21:36 +0100

    reorganize structure

--- src/DependencyInjection/LelandLolExtension.php
+++ src/DependencyInjection/LelandLolExtension.php
@@ -2,6 +2,7 @@
 
 namespace Leland\LolBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
@@ -16,10 +17,13 @@ final class LelandLolExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $processor = new Processor();
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $config = $processor->processConfiguration($configuration, $configs);
         $container->setParameter('leland_lol.tag_class', $config['tag_class']);
         $container->setParameter('leland_lol.purge', $config['purge']);
+        $container->getDefinition('leland_lol');
+
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.xml');
     }
